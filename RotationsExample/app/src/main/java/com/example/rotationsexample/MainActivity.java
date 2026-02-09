8ca03b7d9dc4c78c594e995314a7cae1353f9add
JesungHwang <jesung.hwang@proton.me>
2026-02-09 13:34:27 -0500

    Added appendTime() and onCreate()
    Added a function that detects the orientation and when it happened. It is done on Override onCreate().

--- RotationsExample/app/src/main/java/com/example/rotationsexample/MainActivity.java
+++ RotationsExample/app/src/main/java/com/example/rotationsexample/MainActivity.java
@@ -38,4 +38,26 @@ public class MainActivity extends AppCompatActivity {
         layout.setText(" height = " + (r.bottom - r.top) + " width = " +
                 (r.right - r.left));
     }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        detectDevice();
+        appendTime();
+        Log.d(tag, "onCreate");
+    }
+    public void appendTime() {
+        // lookup the current time
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+        Date date = new Date();
+        // get the current orientation text, append the time
+        // to the text, search for the trailing space.
+        TextView orientation = findViewById(R.id.orientation);
+        String newStr = orientation.getText() + " ";
+        // find the first trailing space
+        int ind = newStr.indexOf(' ');
+        // append the current time
+        newStr = newStr.substring(0, ind+1) + formatter.format(date);
+        orientation.setText(newStr);
+    }
 }
